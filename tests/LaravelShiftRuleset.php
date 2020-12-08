49d2c10a9a1083c7aa7536a28b9f53484fdcdfd3
Trick <trick.developer@gmail.com>
2020-12-08 05:49:49 -0500

    add unit tests for rulesets

--- tests/LaravelShiftRuleset.php
+++ tests/LaravelShiftRuleset.php
@@ -2,18 +2,18 @@
 
 namespace Permafrost\Tests\Unit;
 
-use Permafrost\PhpCsFixerRules\Rulesets\DefaultRuleset;
+use Permafrost\PhpCsFixerRules\Rulesets\LaravelShiftRuleset;
 use Permafrost\PhpCsFixerRules\Rulesets\RuleSet;
 use PHPUnit\Framework\TestCase;
 
-class DefaultRulesetTest extends TestCase
+class LaravelShiftRulesetTest extends TestCase
 {
     /**
      * @test
      */
     public function it_implements_the_ruleset_contract(): void
     {
-        $ruleset = new DefaultRuleset();
+        $ruleset = new LaravelShiftRuleset();
 
         $this->assertInstanceOf(RuleSet::class, $ruleset);
     }
@@ -23,7 +23,8 @@ class DefaultRulesetTest extends TestCase
      */
     public function it_implements_only_interface_methods(): void
     {
-        $reflect = new \ReflectionClass(new DefaultRuleset());
+        $reflect = new \ReflectionClass(new LaravelShiftRuleset());
+
         $this->assertCount(3, $reflect->getMethods(\ReflectionMethod::IS_PUBLIC));
     }
 
@@ -32,11 +33,11 @@ class DefaultRulesetTest extends TestCase
      */
     public function it_returns_a_valid_name(): void
     {
-        $ruleset = new DefaultRuleset();
+        $ruleset = new LaravelShiftRuleset();
 
         $this->assertIsString($ruleset->name());
         $this->assertNotEmpty($ruleset->name());
-        $this->assertEquals('default', strtolower($ruleset->name()));
+        $this->assertEquals('laravel_shift', strtolower($ruleset->name()));
     }
 
     /**
@@ -44,9 +45,10 @@ class DefaultRulesetTest extends TestCase
      */
     public function it_returns_valid_rules(): void
     {
-        $ruleset = new DefaultRuleset();
+        $ruleset = new LaravelShiftRuleset();
 
         $this->assertIsArray($ruleset->rules());
         $this->assertNotEmpty($ruleset->rules());
+        $this->assertGreaterThan(5, count($ruleset->rules()));
     }
 }
--- tests/PhpUnitRuleset.php
+++ tests/PhpUnitRuleset.php
@@ -2,18 +2,18 @@
 
 namespace Permafrost\Tests\Unit;
 
-use Permafrost\PhpCsFixerRules\Rulesets\DefaultRuleset;
+use Permafrost\PhpCsFixerRules\Rulesets\PhpUnitRuleset;
 use Permafrost\PhpCsFixerRules\Rulesets\RuleSet;
 use PHPUnit\Framework\TestCase;
 
-class DefaultRulesetTest extends TestCase
+class PhpUnitRulesetTest extends TestCase
 {
     /**
      * @test
      */
     public function it_implements_the_ruleset_contract(): void
     {
-        $ruleset = new DefaultRuleset();
+        $ruleset = new PhpUnitRuleset();
 
         $this->assertInstanceOf(RuleSet::class, $ruleset);
     }
@@ -23,7 +23,8 @@ class DefaultRulesetTest extends TestCase
      */
     public function it_implements_only_interface_methods(): void
     {
-        $reflect = new \ReflectionClass(new DefaultRuleset());
+        $reflect = new \ReflectionClass(new PhpUnitRuleset());
+
         $this->assertCount(3, $reflect->getMethods(\ReflectionMethod::IS_PUBLIC));
     }
 
@@ -32,11 +33,11 @@ class DefaultRulesetTest extends TestCase
      */
     public function it_returns_a_valid_name(): void
     {
-        $ruleset = new DefaultRuleset();
+        $ruleset = new PhpUnitRuleset();
 
         $this->assertIsString($ruleset->name());
         $this->assertNotEmpty($ruleset->name());
-        $this->assertEquals('default', strtolower($ruleset->name()));
+        $this->assertEquals('php_unit', strtolower($ruleset->name()));
     }
 
     /**
@@ -44,9 +45,10 @@ class DefaultRulesetTest extends TestCase
      */
     public function it_returns_valid_rules(): void
     {
-        $ruleset = new DefaultRuleset();
+        $ruleset = new PhpUnitRuleset();
 
         $this->assertIsArray($ruleset->rules());
         $this->assertNotEmpty($ruleset->rules());
+        $this->assertGreaterThan(5, count($ruleset->rules()));
     }
 }
